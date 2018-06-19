ba94626a3cb9aea4e418bfb03eb3c9e518586a28
Marco Fernandes <marco@front.no>
2018-06-19 11:58:06 +0100

    Cleanup webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,7 @@
 const path = require('path');
-const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const merge = require('webpack-merge');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-function camelCaseDash( string ) {
-	return string.replace(
-		/-([a-z])/,
-		( match, letter ) => letter.toUpperCase()
-	);
-}
-
-const entryPointNames = [
-	'blocks',
-	'components',
-	'date',
-	'editor',
-	'element',
-	'utils',
-	'data',
-	'viewport',
-	'core-data',
-	'plugins',
-	// 'edit-post',
-];
-
-const externals = {};
-
-[
-	...entryPointNames,
-].forEach( ( name ) => {
-	externals[ `@wordpress/${ name }` ] = {
-		this: [ 'wp', camelCaseDash( name ) ],
-	};
-} );
 
 module.exports = {
   entry: './client_src/index.js',
@@ -42,14 +9,13 @@ module.exports = {
     filename: 'js/[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  externals,
   module: {
     rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: 'babel-loader',
-			},
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
@@ -94,25 +60,14 @@ module.exports = {
     ])      
   ],
   optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          chunks: "initial",
-          test: path.resolve(__dirname, "node_modules"),
-          name: "vendor",
-          enforce: true
-        }
-      }
-    }
-  },optimization: {
     // Not used for now. Vendor css files are imported on
     // the main css file.
     splitChunks: {
       cacheGroups: {
         vendor: {
-          chunks: "initial",
-          test: path.resolve(__dirname, "node_modules"),
-          name: "vendor",
+          chunks: 'initial',
+          test: path.resolve(__dirname, 'node_modules'),
+          name: 'vendor',
           enforce: true
         }
       }
