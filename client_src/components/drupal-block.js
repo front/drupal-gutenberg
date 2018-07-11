775afff93262c3e3b7656719602f1c83127f721d
Marco Fernandes <marcofernandes75@gmail.com>
2018-07-11 10:31:06 +0100

    Register Drupal blocks on Gutenberg.

---
+++ client_src/components/drupal-block.js
@@ -0,0 +1,30 @@
+import { data } from '@frontkom/gutenberg';
+
+const { withSelect } = data;
+
+class DrupalBlock extends wp.element.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.blockContent) {
+      return (
+        <div className={this.props.className} dangerouslySetInnerHTML={{__html: this.props.blockContent.html}} />
+      );
+    }
+
+    return(
+      <div className="loading-drupal-block">{Drupal.t('Loading')}...</div>
+    );
+  }
+}
+
+export default withSelect((select, props) => {
+  const { getBlock } = select('drupal');
+  const { id } = props;
+
+  return {
+    blockContent: getBlock(id) // `/editor/blocks/load/${blockId}`
+  };
+})( DrupalBlock );
