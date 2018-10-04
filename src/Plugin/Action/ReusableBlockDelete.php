206a03cfbb63c0f44645529667f5865d0ac5f15c
Marco Fernandes <marcofernandes75@gmail.com>
2018-10-04 09:19:51 +0100

    Issue #3002137: Override access for reusable block.
    + Add missing config files.

--- src/Plugin/Action/ReusableBlockDelete.php
+++ src/Plugin/Action/ReusableBlockDelete.php
@@ -34,10 +34,11 @@ class ReusableBlockDelete extends ActionBase {
    */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
     /** @var \Drupal\block_content\BlockContentInterface $object */
-    $result = $object->access('update', $account, TRUE)
-      ->andIf($object->field_push->access('edit', $account, TRUE));
+    // $result = $object->access('update', $account, TRUE)
+    //   ->andIf($object->field_push->access('edit', $account, TRUE));
 
-    return $return_as_object ? $result : $result->isAllowed();
+    // return $return_as_object ? $result : $result->isAllowed();
+    return TRUE;
   }
 
 }
